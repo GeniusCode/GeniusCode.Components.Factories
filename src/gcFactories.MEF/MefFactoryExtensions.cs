fc43f6b48f0f7483c7ea6cba5e96ab1f66edfe4c
jeremiah@geniuscode.net <Jeremiah@geniuscode.net>
2011-08-29 14:25:34 -0700

    added extension methods

---
+++ src/gcFactories.MEF/MefFactoryExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition.Hosting;
+
+namespace GeniusCode.Components
+{
+    public class MefFactoryExtensions
+    {
+
+        public static void AddMefLocatorProvider<T, TMefContract, TMetadata>(this IList<IFactory<T>> input,
+            CompositionContainer container,
+            Func<object, IEnumerable<Lazy<TMefContract, TMetadata>>, Lazy<TMefContract, TMetadata>> selector,
+            Func<object, Lazy<TMefContract, TMetadata>, bool> predicate, bool shareInstances)
+            where T : class, TMefContract
+            where TMefContract : class
+            where TMetadata : class
+        {
+            var mp = new MefInstanceProvider<T, TMefContract, TMetadata>(container, shareInstances) { Predicate = predicate, Selector = selector };
+            input.Add(mp);
+        }
+
+
+        public static void AddMefLocatorProvider<T, TMefContract, TMetadata, TArgs>(this IList<IFactory<T>> input,
+        CompositionContainer container,
+        Func<TArgs, IEnumerable<Lazy<TMefContract, TMetadata>>, Lazy<TMefContract, TMetadata>> selector,
+        Func<TArgs, Lazy<TMefContract, TMetadata>, bool> predicate, bool shareInstances)
+            where T : class, TMefContract
+            where TMefContract : class
+            where TMetadata : class
+            where TArgs : class
+        {
+            var mp = new MefInstanceProvider<T, TMefContract, TMetadata, TArgs>(container, shareInstances) { Predicate = predicate, Selector = selector };
+            input.Add(mp);
+        }
+
+    }
+}
---
+++ src/gcFactories/FactoryExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using GeniusCode.Components.Factories;
+
+namespace GeniusCode.Components
+{
+    public static class FactoryExtensions
+    {
+        public static void AddNewDefaultConstructorFactory<T>(this IList<IFactory<T>> input) where T : class, new()
+        {
+            input.Add(new DefaultConstructorFactory<T>());
+        }
+    }
+}
